00361e08d3555fa96d897ba0660f4c3b4b0e3a9f
Raksha1703 <rakshabarai173@gmail.com>
2021-03-16 16:38:16 +0530

    user created

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public message : string ="";
+  public username : string ="";
+  public password : string ="";
+
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    this.userService.login(this.username, this.password)
+    .subscribe((resp)=>{
+      console.log('Succecfully Logging in');
+      this.message = resp.msg;
+    },(err) => {
+      console.error('Error logging in',err);
+      this.message = err.error.msg;
+    });
+  }
 }
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public username:string = "";
+  public password:string = "";
+  public message:string = "";
+
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
   }
 
+  register(){
+    this.userService.register(this.username, this.password)
+    .subscribe((resp) => 
+    { console.log('Successfully Register');
+      this.message = resp.msg;
+    },(err) => 
+    { console.error('Error Registering', err)
+      this.message = err.error.msg;      
+    });
+  }
 }
